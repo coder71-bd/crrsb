c69dbe250c90dac0ca876e029c55709ca2b4d525
jubayer__hossain <jubayer.dev71@gmail.com>
2024-06-16 20:23:10 +0600

    eslint prettier setup

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    languageOptions: { globals: globals.node },
+    rules: {
+      "no-console": "warn",
+      "no-undef": "error",
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+    ignores: ["**/node_modules/", "**/dist/"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];
